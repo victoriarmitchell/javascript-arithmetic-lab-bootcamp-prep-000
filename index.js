ce1f3dfef72bdc71d7951bf30e745310fab042ce
Victoria M <victoria.raq.mitchell@gmail.com>
2019-04-02 01:04:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,11 +25,11 @@ function dec(n) {
 }
 
 function makeInt(n) {
-  parseInt('n',10);
+  parseInt(n,10);
   return n;
 }
 
 function preserveDecimal(n) {
-  parseFloat('n');
+  parseFloat(n);
   return n;
 }
